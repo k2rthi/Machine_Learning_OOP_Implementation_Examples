42e2675f8880ae9f47eeda39ce47e1755a87ce82
“k2rthi” <karthiheyan.eswaran@education.nsw.gov.au>
2025-07-19 19:53:49 +1000

    check

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+from sklearn.linear_model import LinearRegression
+
+from sklearn.metrics import mean_absolute_error,mean_squared_error 
