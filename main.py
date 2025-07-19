963da4abffd23c66f91b924e279d8926e01883ff
“k2rthi” <karthiheyan.eswaran@education.nsw.gov.au>
2025-07-19 19:56:10 +1000

    check 2

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ plt.style.use('ggplot')
 from sklearn.linear_model import LinearRegression
 
 from sklearn.metrics import mean_absolute_error,mean_squared_error 
+
+#Import data as a Pandas DataFrame
+insects = pd.read_csv('./data/insects.csv', sep='\t')
+insects = pd.DataFrame({
+    'continent': insects['continent'],
+    'latitude': insects['latitude'],
+    'sex': insects['sex'],
+    'wingsize': insects['wingsize']
+})
+
+# Filter the data to only male insects
+insects = insects[insects.sex == 1]
+
+# Features variable
+X_insects = insects[['wingsize']]
+# Target variable
+y_insects = insects['latitude']
+
+insects.head()
