0166b3dea10b1d078c772d54fe910bf9b1dea41b
“k2rthi” <karthiheyan.eswaran@education.nsw.gov.au>
2025-07-21 17:28:23 +1000

    still not working

---
+++ main2.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+from sklearn.linear_model import LinearRegression
+
+from sklearn.metrics import mean_absolute_error,mean_squared_error 
+
+#Import data as a Pandas DataFrame
+insects = pd.read_csv('./data/insects.csv', sep='\t')
+insects = pd.DataFrame({
+    'continent': insects['continent'],
+    'latitude': insects['latitude'],
+    'sex': insects['sex'],
+    'wingsize': insects['wingsize']
+})
+
+# Features variable
+X_insects = insects[['continent', 'latitude', 'sex']]
+# Target variable
+y_insects = insects['wingsize']
+
+insects.head()
+
+# Plot the data, colour coded by sex
+ax = plt.scatter(insects.latitude, insects.wingsize, c=insects['sex'])
+plt.xlabel("Wingsize")
+plt.ylabel("Lattitude")
+plt.colorbar()
+plt.show()
+
+insects_regression = LinearRegression()
+
+insects_regression.fit(X_insects, y_insects)
+
+# Predict the target for the whole dataset
+wing_size_predictions = insects_regression.predict(X_insects)
+
+#Predict the target for a new data point
+new_insect = pd.DataFrame({
+    'continent': [1],
+    'latitude': [40],
+    'sex': [0]
+})
+new_insect['wingsize'] = insects_regression.predict(new_insect)
+print(f"New insect is:\n{new_insect}")
+
+#Plot the predictions compared to the actual data
+fig, axs = plt.subplots(1, 2, figsize=(14, 5))
+
+axs[0].scatter(insects.latitude, insects.wingsize, c=insects['sex'],  s=40)
+axs[0].set_title("Actual Data")
+axs[0].set_xlabel("Latitude")
+axs[0].set_ylabel("Actual Wing Span")
+
+
+axs[1].scatter(insects.latitude, wing_size_predictions, c=insects['sex'], s=40)
+axs[1].set_title("Predicted Data")
+axs[1].set_xlabel("Latitude")
+axs[1].set_ylabel("Predicted Wing Span")
+
+plt.show()
+
+#Get Evalutative Data from the model
+print(f"Model coefficient :{insects_regression.coef_}")
+print(f"Model y intercept :{insects_regression.intercept_}")
+print(f"Model score :{insects_regression.score(X_insects,y_insects)}")
+mae = mean_absolute_error(y_true=y_insects,y_pred=wing_size_predictions) 
+mse = mean_squared_error(y_true=y_insects,y_pred=wing_size_predictions)
+print("MAE:",mae)
+print("MSE:",mse)
+
